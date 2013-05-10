f622fd0b1055f89bcd7b76bd2b3392dba103fa5f
eshion <eshion@qq.com>
2013-05-10 12:57:24 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -4,7 +4,7 @@ HOST=$1
 PORT=$2
 USER=$3
 PASS=$4
-ACTION=$5
+CMD=$5
 
 expect -c "
 set timeout 5
@@ -12,7 +12,7 @@ spawn ssh -l${USER} -p${PORT} ${HOST}
 expect \"*assword:\"
 send ${PASS}\r
 expect \":~>\"
-send \"${ACTION}\r\"
+send \"${CMD}\r\"
 expect \":~>\"
 send \"exit\r\"
 interact
